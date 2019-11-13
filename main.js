fada2dc5144f8bafa404b86d0854483aa8a201a1
fedPass <federica.passanante@gmail.com>
2019-11-13 17:09:51 +0100

    corretta sintassi

--- main.js
+++ main.js
@@ -10,5 +10,5 @@ var name = prompt('Inserisci il tuo nome:');
 var surname = prompt('Inserisci il tuo cognome:');
 var bestColor = prompt('Inserisci il tuo colore preferito:');
 console.log(name + surname + bestColor);
-document.getElementById('show_password').innerHTML = name + surname + bestColor + '19'
-document.getElementById('container').setAttribute('class','visible')
+document.getElementById('show_password').innerHTML = name + surname + bestColor + '19';
+document.getElementById('container').setAttribute('class','visible');
