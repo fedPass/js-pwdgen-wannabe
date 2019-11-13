e6963850377350a0a70558f1d3264c10b7ae911b
fedPass <federica.passanante@gmail.com>
2019-11-13 16:31:33 +0100

    inserito html

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 //prova se è correttamente collegato
-alert('Questa è una prova!')
+// alert('Questa è una prova!')
+
+/*ES:
+1. chiedere attraverso prompt [nome][cognome][colorePreferito]
+2. generare psw formata da [nome][cognome][colorePreferito]19
+3. visualizzarla a schermo
+*/
+var name = prompt('Inserisci il tuo nome:');
+var surname = prompt('Inserisci il tuo cognome:');
+var bestColor = prompt('Inserisci il tuo colore preferito:');
+console.log(name + surname + bestColor);
+document.getElementById('show_password').innerHTML = name + surname + bestColor + '19'
